ec02072941d4d06f6431cf4e555c8504a8d939a3
Tylar <murray.tylar@gmail.com>
2024-05-21 11:22:56 -0400

    initial setup

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # data_reports
 Quarto reports generated to show status of MBON-mediated datasets.
+
+## Usage
+1. export the [MBON datasets spreadsheet](https://docs.google.com/spreadsheets/d/1jBS8ASS27yV8APZ8Fh-tgX6dHdopwianrUZv0kbKcxw/edit#gid=1284796732) as `.csv` & save to `./data/MBON dataset registration (Responses) - Form Responses 1.csv`
+2. `quarto render .` NOTE: also try `quarto render . --no-cache`
+3. `quarto preview .`
+4. `quarto publish .`
---
+++ prerender_report_templates.R
@@ -0,0 +1,37 @@
+# creates a report template .qmd for each
+REPORT_TEMPLATE = "regional_report/regional_report.qmd"
+REPORTS_DIR = "regional_report/regional_reports"
+
+library(whisker)
+library(glue)
+
+# Proceed if rendering the whole project, exit otherwise
+if (!nzchar(Sys.getenv("QUARTO_PROJECT_RENDER_ALL"))) {
+  quit()
+}
+
+# create the template
+templ <- readLines(REPORT_TEMPLATE) 
+templ <- gsub(
+  "SE US", "{{project_id}}", templ
+)
+
+dir.create(REPORTS_DIR, showWarnings=FALSE)
+
+# === iterate through the data structure 
+data <- read.csv("data/MBON dataset registration (Responses) - Form Responses 1.csv")
+
+# print unique values in `Parameter` column
+unique_parameters <- unique(data$`Which.MBON.project.is.this.dataset.associated.with.`)
+
+for (param in unique_parameters) {
+  params = list(
+    project_id = param
+  )
+  print(glue::glue("=== creating template for '{param}' ==="))
+  
+  writeLines(
+    whisker::whisker.render(templ, params),
+    file.path(glue("{REPORTS_DIR}/{param}.qmd"))
+  )
+}
