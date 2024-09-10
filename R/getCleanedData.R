60a908542c9be85677c89150fdb1df2168255103
Tylar <murray.tylar@gmail.com>
2024-09-10 15:00:19 -0400

    modify table to have links

--- R/getCleanedData.R
+++ R/getCleanedData.R
@@ -7,9 +7,21 @@ getCleanedData <- function(){
     rename(  # shorten column names
       mbon.project = Which.MBON.project.is.this.dataset.associated.with.,
       RA = If.you.have.worked.with.a.Regional.Association..please.indicate.which.one.s..,
+      
       erddap = Has.the.dataset.been.loaded.into.ERDDAP.,
+      erddap_link = If.yes.to.above..please.include.appropriate.ERDDAP.link.s..here.,
+
       obis = Has.the.dataset.been.submitted.to.OBIS.,
-      ncei = Has.the.dataset.been.archived.at.NCEI.
+      obis_link = If.yes.to.above..please.include.appropriate.OBIS.link.s..here.,
+
+      ncei = Has.the.dataset.been.archived.at.NCEI.,
+      ncei_link = If.yes.to.above..please.include.appropriate.NCEI.link.s..here.,
+      
+      web = Are.the.data.accessible.via.the.web.,
+      web_link = If.yes.to.above..please.include.appropriate.link.s..here.,
+      
+      code = Are.there.scripts.or.code.used.to.process.the.data.,
+      code_link = If.yes.to.above..and.the.code.is.available..please.include.appropriate.link.s..here.
     ) %>%  # trim whitespace on edges
     mutate(across(where(is.character), trimws))
  
